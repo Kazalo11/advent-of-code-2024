7b8a77d438ad1e9cf09a875f0167190fd70aadad
Kazal Oshodi <kazoshodi@gmail.com>
2024-12-04 22:30:31 +0000

    part 1

--- day4/day4.go
+++ day4/day4.go
@@ -16,6 +16,17 @@ var columnHeight int
 type Coordinate struct{ row, column int }
 
 var solutionPaths [][]Coordinate
+var count int
+var directions = map[string][2]int{
+	"down-right": {1, 1},
+	"down":       {1, 0},
+	"down-left":  {1, -1},
+	"right":      {0, 1},
+	"left":       {0, -1},
+	"up-right":   {-1, 1},
+	"up":         {-1, 0},
+	"up-left":    {-1, -1},
+}
 
 func init() {
 
@@ -43,16 +54,14 @@ func getData() [][]string {
 
 }
 
-func nextLetter(index int, row int, column int, matrix [][]string, path []Coordinate) bool {
+func nextLetter(index int, row int, column int, matrix [][]string, path []Coordinate, direction string) bool {
 	if row < 0 || column < 0 || row >= rowWidth || column >= columnHeight {
 		return false
 	}
 
-	currentLetter := matrix[row][column]
+	// fmt.Printf("Current path for %d, %d is %v \n", row, column, path)
 
-	if (currentLetter == "X" && index != 0) || (currentLetter != "X" && index == 0) {
-		return false
-	}
+	currentLetter := matrix[row][column]
 
 	if currentLetter == "S" && index == len(xmas)-1 {
 		coordinate := Coordinate{row: row, column: column}
@@ -63,7 +72,6 @@ func nextLetter(index int, row int, column int, matrix [][]string, path []Coordi
 	}
 
 	expectedLetter := xmas[index]
-	// fmt.Printf("Current letter is %s, expected Letter is %s \n", currentLetter, expectedLetter)
 
 	if currentLetter != expectedLetter {
 		return false
@@ -71,31 +79,33 @@ func nextLetter(index int, row int, column int, matrix [][]string, path []Coordi
 
 	coordinate := Coordinate{row: row, column: column}
 	path = append(path, coordinate)
+	dir := directions[direction]
+	newRow, newCol := row+dir[0], column+dir[1]
 
-	return nextLetter(index+1, row+1, column+1, matrix, path) ||
-		nextLetter(index+1, row+1, column, matrix, path) ||
-		nextLetter(index+1, row+1, column-1, matrix, path) ||
-		nextLetter(index+1, row, column+1, matrix, path) ||
-		nextLetter(index+1, row, column-1, matrix, path) ||
-		nextLetter(index+1, row-1, column+1, matrix, path) ||
-		nextLetter(index+1, row-1, column, matrix, path) ||
-		nextLetter(index+1, row-1, column-1, matrix, path)
+	return nextLetter(index+1, newRow, newCol, matrix, path, direction)
 
 }
 
-func main() {
-	// count := 0
+func part1() {
+
 	matrix := getData()
 	for rowIndex, row := range matrix {
 		for column := range row {
 			path := make([]Coordinate, 0)
-			// path = append(path, coordinate)
-			// if nextLetter(0, rowIndex, column, matrix, path) {
-			// 	count++
-			// }
-			nextLetter(0, rowIndex, column, matrix, path)
+
+			start_letter := matrix[rowIndex][column]
+			if start_letter == "X" {
+				for direction := range directions {
+
+					nextLetter(0, rowIndex, column, matrix, path, direction)
+				}
+			}
+
 		}
 	}
-	fmt.Print(len(solutionPaths))
+	fmt.Printf("Answer to part 1: %d \n", len(solutionPaths))
 
+}
+func main() {
+	part1()
 }
