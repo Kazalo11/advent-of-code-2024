d22cba938651d393486d371f4a4a12da84a414cc
Kazal Oshodi <kazoshodi@gmail.com>
2024-12-06 21:00:42 +0000

    work

--- day6/character.go
+++ day6/character.go
@@ -5,7 +5,7 @@ type Character struct {
 	location  [2]int
 }
 
-func (c *Character) NewCharacter(location [2]int) *Character {
+func NewCharacter(location [2]int) *Character {
 	return &Character{
 		direction: 0,
 		location:  location,
@@ -14,4 +14,48 @@ func (c *Character) NewCharacter(location [2]int) *Character {
 
 func (c *Character) TurnDirection() {
 	c.direction = (c.direction + 1) % 4
+}
+
+func (c *Character) GetLocation() [2]int {
+	return c.location
+}
+
+func (c *Character) GetDirection() Direction {
+	return c.direction
+}
+
+func (c *Character) Up() {
+	c.location[0] = c.location[0] - 1
+}
+
+func (c *Character) Down() {
+	c.location[0] = c.location[0] + 1
+}
+
+func (c *Character) Left() {
+	c.location[1] = c.location[1] - 1
+}
+
+func (c *Character) Right() {
+	c.location[1] = c.location[1] + 1
+}
+
+func (c *Character) LookAhead(m Matrix) string {
+	curr := c.GetLocation()
+	row := curr[0]
+	col := curr[1]
+	switch c.GetDirection() {
+	case FORWARD:
+		if row == 0 {
+
+		}
+		return m.data[row-1][col]
+	case BACKWARD:
+		guard.Down()
+	case LEFT:
+		guard.Left()
+	case RIGHT:
+		guard.Right()
+
+	}
 }
--- day6/part1.go
+++ day6/part1.go
@@ -3,7 +3,18 @@ package main
 func part1() {
 	matrix := NewMatrix("day6/day6.txt")
 	guardLocation := matrix.FindGuard()
-	guard := NewCharacter{
-		location: guardLocation,
+	guard := NewCharacter(guardLocation)
+	for {
+		switch guard.GetDirection() {
+		case FORWARD:
+			guard.Up()
+		case BACKWARD:
+			guard.Down()
+		case LEFT:
+			guard.Left()
+		case RIGHT:
+			guard.Right()
+
+		}
 	}
 }
