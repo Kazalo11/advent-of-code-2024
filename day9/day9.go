d4b9f8139f9fd5913a49f1c447a438cd8083775d
Kazal Oshodi <kazoshodi@gmail.com>
2024-12-09 17:55:26 +0000

    stuff

--- day9/day9.go
+++ day9/day9.go
@@ -22,8 +22,8 @@ func getData() string {
 	return line
 }
 
-func constructBlocks(line string) string {
-	var ans string
+func constructBlocks(line string) []rune {
+	var ans []rune
 	id := 0
 	for index, charRune := range line {
 		char := string(charRune)
@@ -34,13 +34,13 @@ func constructBlocks(line string) string {
 		if index%2 == 0 {
 
 			for i := 0; i < num; i++ {
-				ans += fmt.Sprint(id)
+				ans = append(ans, rune(id))
 			}
 
 			id++
 		} else {
 			for i := 0; i < num; i++ {
-				ans += "."
+				ans = append(ans, '.')
 			}
 		}
 
@@ -50,29 +50,23 @@ func constructBlocks(line string) string {
 	return ans
 }
 
-func calculateCheckSum(block string) int {
+func calculateCheckSum(block []rune) int {
 	ans := 0
 	p1 := 0
 	p2 := len(block) - 1
-	for p1 <= p2 {
-		char := string(block[p1])
-		if char != "." {
-			num, err := strconv.Atoi(char)
-			if err != nil {
-				panic(fmt.Sprintf("Can't convert %s to num", char))
-			}
-			ans += (num * p1)
+	for p1 < p2 {
+		char := block[p1]
+		if string(char) != "." {
+
+			ans += (int(char) * p1)
 		} else {
-			char2 := string(block[p2])
-			for char2 == "." {
+			char2 := block[p2]
+			for string(char2) == "." {
 				p2--
-				char2 = string(block[p2])
+				char2 = block[p2]
 			}
-			num, err := strconv.Atoi(char2)
-			if err != nil {
-				panic(fmt.Sprintf("Can't convert %s to num", char2))
-			}
-			ans += (num * p1)
+
+			ans += (int(char2) * p1)
 			p2--
 
 		}
